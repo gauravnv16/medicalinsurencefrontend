ff84ec87a3abdeb8752131286f44ea6e28197ffb
GAURAV N V <114969570+gauravnv16@users.noreply.github.com>
2023-04-21 10:43:43 +0530

    Update API.jsx

--- frontend/API.jsx
+++ frontend/API.jsx
@@ -1,3 +1,3 @@
-export const API_URL="https://medicalinsurencebackend.herokuapp.com/"
+export const API_URL="https://medicalinsurencebackend.onrender.com/"
 //export const API_URL="http://localhost:8000/"
 
